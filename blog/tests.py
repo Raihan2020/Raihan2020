a06d01025d5d81bbf0857093a6b044aba9c369b2
Raihan Rahman <rdyingr@gmail.com>
2024-12-22 23:29:58 -0800

    initial commit

---
+++ blog/tests.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Post
+from django.urls import reverse
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
+        cls.post = Post.objects.create(
+            title="A good title",
+            body="Nice body content",
+            author=cls.user,
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "A good title")
+        self.assertEqual(self.post.body, "Nice body content")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "A good title")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+    
+    def url_exist_at_correct_location_listview(self):
+        response = self.client.get('/')
+        return self.assertEqual(response.status_code,200)
+    
+    def url_exist_at_correct_location_detailview(self):
+        response = self.client.get('/post/1/')
+        return self.assertEqual(response.status_code,200)
+    
+    def test_post_listview(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response,'Nice body content')
+        self.assertTemplateUsed(response,"home.html")
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse('post_detail',kwargs={"pk":self.post.pk}))
+        no_response = self.client.get("/post/10000/")
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(no_response.status_code,404)
+        self.assertContains(response,"A good title")
+        self.assertTemplateUsed(response,"post_detail.html")
